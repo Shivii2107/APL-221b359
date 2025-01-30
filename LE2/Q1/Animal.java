7e14fe414470a7f2ceffaecccf20d39c9bd20447
Shivani Singh <221b359@gmail.com>
2025-01-30 20:53:08 +0530

    LE2

---
+++ LE2/Q1/Animal.java
@@ -0,0 +1,5 @@
+class Animal {
+    void makeVoice() {
+        System.out.println("This is a generic animal sound.");
+    }
+}
---
+++ LE2/Q2/Voice2.java
@@ -0,0 +1,6 @@
+class Voice2 extends Voice {
+    public void templateMethod() {
+        prepareVoice();
+        hear();
+    }
+}
