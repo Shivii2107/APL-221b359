15a50c2dd2fa505164b857205bd7017f3ec6361e
Shivani Singh <221b359@gmail.com>
2025-02-20 12:14:46 +0530

    Initial Commit

---
+++ LE2/Q3/com/jiet/Pack2.java
@@ -0,0 +1,10 @@
+package com.jiet;
+
+import com.juet.Pack1;
+
+public class Pack2 extends Pack1 {
+    public static void main(String[] args) {
+        Pack2 obj = new Pack2();
+        obj.display();
+    }
+}
---
+++ LE2/Q3/com/juet/Pack1.java
@@ -0,0 +1,7 @@
+package com.juet;
+
+public class Pack1 {
+    protected void display() {
+        System.out.println("Display method of Pack1");
+    }
+}
